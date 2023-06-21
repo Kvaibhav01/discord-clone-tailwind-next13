a48b5272107af5ba0eba39da19f3551945519d2a
Vaibhav <khulbevaibhavdev@gmail.com>
2023-06-21 18:02:59 +0530

    Changes base styles and added server selector

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.css';
+import './../styles.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Discord Clone</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,34 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Whitney', 'Open Sans', ...defaultTheme.fontFamily.sans],
+        title: ['Ginto', 'Open Sans', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        brand: '#5866F2',
+        gray: {
+          50: '#ECEDEE',
+          100: '#DCDDDE',
+          200: '#B9BBBE',
+          300: '#8E9297',
+          400: '#72767D',
+          500: '#5C6067',
+          600: '#464950',
+          700: '#36393F',
+          800: '#2F3136',
+          900: '#202225',
+        },
+      },
+    },
   },
   plugins: [],
 };
